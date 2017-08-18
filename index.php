0d535b8f2abd38a1ab8f51355ef09abd95d4e9c1
otu02 <ellipseshub@gmail.com>
2017-08-18 14:21:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,16 @@ function fullname() {
 }
 
 ?>
-
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home Page</title>
+</head>
+<body>
+  <header>
+    <h2>Welcome Student Profile system</h2>
   
   
   

4a5f1093563aa87da6db2f1982be4d2d6565963f
otu02 <ellipseshub@gmail.com>
2017-08-18 13:15:53 +0100

    Delete config.php

--- includes/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Database constants
-
-defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
-defined('DB_USER') ? null : define("DB_USER", "admin");
-defined('DB_PASS') ? null : define("DB_PASS", "123abc");
-defined('DB_NAME') ? null : define("DB_NAME", "std_details");

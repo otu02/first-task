f5638e616e390957c20b78b5fe469555d56dd925
otu02 <ellipseshub@gmail.com>
2017-08-18 13:15:20 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+//Database setup
+defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
+defined('DB_USER') ? null : define("DB_USER", "shield");
+defined('DB_PASS') ? null : define("DB_PASS", "1234abc");
+defined('DB_NAME') ? null : define("DB_NAME", "globalpairs");
+
